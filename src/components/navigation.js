d05da1cf36926879aa11cb56c4ca87b1fa9e2cdb
Daniel Páez <danielpaezsw66@gmail.com>
2019-11-28 23:02:34 -0300

    Content added, pages created

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -11,6 +11,12 @@ export default () => (
       <li className={styles.navigationItem}>
         <Link to="/blog/">Blog</Link>
       </li>
+      <li className={styles.navigationItem}>
+        <Link to="/nuestros-principios/">Nuestros Principios</Link>
+      </li>
+      <li className={styles.navigationItem}>
+        <Link to="/tu-perspectiva/">Necesitamos tu perspectiva</Link>
+      </li>
     </ul>
   </nav>
 )
